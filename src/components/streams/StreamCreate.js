80f57e8ac7372c9105721b5e07f14f88cd03f3b3
TomekMichalski <tmm.michalski@gmail.com>
2019-07-14 15:59:27 +0200

    Implemented validation to StreamCreate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,6 +1,59 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+
+  renderError({error, touched}) {
+    if (error && touched) {
+      return (
+        <div className="ui error message">
+          <div className="header">
+            {error}
+          </div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label htmlFor="">{label}</label>
+        <input {...input} autoComplete="off"/>
+        {this.renderError(meta)}
+      </div>
+      )
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render () {
+    return (
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name="title" component={this.renderInput} label="Enter title"/>
+        <Field name="description" component={this.renderInput} label="Enter description"/>
+
+        <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if(!formValues.title) {
+    errors.title = 'You must enter a valid title';
+  }
+  if(!formValues.description) {
+    errors.description = 'You must enter a valid description';
+  }
+  return errors;
 };
-export default StreamCreate;
+
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -8,11 +8,8 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
-      console.log('test');
       return { ...state, isSignedIn: true, userId: action.payload };
     case SIGN_OUT:
-        console.log('test');
-
       return { ...state, isSignedIn: false, userId: null };
     default:
       return { ...state };
